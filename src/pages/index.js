59a1811ef9994fab3f89b369c564d3dc9c021c4c
劉邦後代 <laubonghaudoi@icloud.com>
2019-08-05 12:02:18 -0700

    Refactor footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import { Link } from 'gatsby';
 
 import HomepageLayout from '../layouts/HomepageLayout';
 import SchemaList from '../components/SchemaList';
+import Footer from '../components/Footer';
 
 const IndexPage = () => (
   <div>
@@ -72,40 +73,7 @@ const IndexPage = () => (
         </Container>
       </Segment>
 
-      <Segment inverted vertical style={{ padding: '5em 0em' }}>
-        <Container>
-          <Grid divided inverted stackable>
-            <Grid.Row>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='About' />
-                <List link inverted>
-                  <List.Item as='a'>Sitemap</List.Item>
-                  <List.Item as='a'>Contact Us</List.Item>
-                  <List.Item as='a'>Religious Ceremonies</List.Item>
-                  <List.Item as='a'>Gazebo Plans</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='Services' />
-                <List link inverted>
-                  <List.Item as='a'>Banana Pre-Order</List.Item>
-                  <List.Item as='a'>DNA FAQ</List.Item>
-                  <List.Item as='a'>How To Access</List.Item>
-                  <List.Item as='a'>Favorite X-Men</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header as='h4' inverted>
-                  Footer Header
-                </Header>
-                <p>
-                  Extra space for a call to action inside the footer that could help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-      </Segment>
+      <Footer/>
     </HomepageLayout>
   </div>
 )
