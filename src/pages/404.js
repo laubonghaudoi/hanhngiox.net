eae76c51dc46505a1416cc80edb8ce04907a47ab
laubonghaudoi <laubonghaudoi@icloud.com>
2019-08-07 22:29:22 -0700

    Ready the examples, and big change

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,150 @@
-import React from 'react'
-import PageLayout from '../layouts/PageLayout'
+    
+import React, { Component } from "react";
 
-const NotFoundPage = () => (
-  <PageLayout>
-    <h2><span role="img" aria-label="Forbidden sign">🚫</span> Not found</h2>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </PageLayout>
-)
+// import "semantic-ui-css/semantic.min.css";
 
-export default NotFoundPage
+import {
+  Button,
+  Container,
+  Dropdown,
+  Grid,
+  Header,
+  Icon,
+  Menu,
+  Message
+} from "semantic-ui-react";
+
+import "./App.css";
+
+class NotFoundPage extends Component {
+  state = {
+    dropdownMenuStyle: {
+      display: "none"
+    }
+  };
+
+  handleToggleDropdownMenu = () => {
+    let newState = Object.assign({}, this.state);
+    if (newState.dropdownMenuStyle.display === "none") {
+      newState.dropdownMenuStyle = { display: "flex" };
+    } else {
+      newState.dropdownMenuStyle = { display: "none" };
+    }
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Grid padded className="tablet computer only">
+          <Menu inverted borderless fluid size="huge">
+            <Container>
+              <Menu.Item header as="a">
+                Project Name
+              </Menu.Item>
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
+              <Dropdown item text="Dropdown">
+                <Dropdown.Menu>
+                  <Dropdown.Item as="a" href="#root">
+                    Action
+                  </Dropdown.Item>
+                  <Dropdown.Item as="a" href="#root">
+                    Another Action
+                  </Dropdown.Item>
+                  <Dropdown.Item as="a" href="#root">
+                    Something else here
+                  </Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Navbar header</Dropdown.Header>
+                  <Dropdown.Item as="a" href="#root">
+                    Separated link
+                  </Dropdown.Item>
+                  <Dropdown.Item as="a" href="#root">
+                    One more separated link
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Menu.Menu position="right">
+                <Menu.Item as="a">Default</Menu.Item>
+                <Menu.Item active as="a">
+                  Static top
+                </Menu.Item>
+                <Menu.Item as="a">Fixed top</Menu.Item>
+              </Menu.Menu>
+            </Container>
+          </Menu>
+        </Grid>
+        <Grid padded className="mobile only">
+          <Menu borderless fluid size="huge">
+            <Menu.Item header as="a">
+              Project Name
+            </Menu.Item>
+            <Menu.Menu position="right">
+              <Menu.Item>
+                <Button
+                  icon
+                  basic
+                  toggle
+                  onClick={this.handleToggleDropdownMenu}
+                >
+                  <Icon name="content" />
+                </Button>
+              </Menu.Item>
+            </Menu.Menu>
+            <Menu
+              vertical
+              borderless
+              fluid
+              style={this.state.dropdownMenuStyle}
+            >
+              <Menu.Item active as="a">
+                Home
+              </Menu.Item>
+              <Menu.Item as="a">About</Menu.Item>
+              <Menu.Item as="a">Contact</Menu.Item>
+              <Dropdown text="Dropdown" className="item">
+                <Dropdown.Menu>
+                  <Dropdown.Item as="a">Action</Dropdown.Item>
+                  <Dropdown.Item as="a">Another action</Dropdown.Item>
+                  <Dropdown.Item as="a">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Header>Navbar header</Dropdown.Header>
+                  <Dropdown.Item as="a">Seperated link</Dropdown.Item>
+                  <Dropdown.Item as="a">One more seperated link</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Menu.Item as="a">Default</Menu.Item>
+              <Menu.Item active as="a">
+                Static top
+              </Menu.Item>
+              <Menu.Item as="a">Fixed top</Menu.Item>
+            </Menu>
+          </Menu>
+        </Grid>
+        <Container>
+          <Message>
+            <Header size="huge" as="h1">
+              Navbar example
+            </Header>
+            <p className="lead">
+              This example is a quick exercise to illustrate how the default,
+              static navbar and fixed to top navbar work. It includes the
+              responsive CSS and HTML, so it also adapts to your viewport and
+              device.
+            </p>
+            <Button primary size="big" as="a">
+              View navbar docs &raquo;
+            </Button>
+          </Message>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
