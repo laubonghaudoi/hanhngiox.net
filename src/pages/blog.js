1da19b19d8daa2f8c24ae060b075bb581059ed53
laubonghaudoi <laubonghaudoi@icloud.com>
2019-08-13 23:56:55 -0700

    Add blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -15,14 +15,13 @@ import {
 
 import PageLayout from "../layouts/PageLayout";
 
-export default ({ data }) => {
-  console.log(data)
+export default () => {
   return (
     <PageLayout>
       <Grid container stackable>
         <Grid.Row>
           <Segment basic>
-            <Header as="h1" size="massive">
+            <Header as="h1" size="massive" href='/blog/write_cantonese'>
               杂谈
               <Divider hidden />
               <Header.Subheader>
@@ -64,12 +63,13 @@ export default ({ data }) => {
                 正體字
               </Label>
             </Header>
+            <Header.Subheader>阿擇 （Chaaak） 2019.2.24</Header.Subheader>
             <Divider hidden />
             <blockquote>
               為大家解決幾個關於粵語書寫嘅困惑
             </blockquote>
             <Segment secondary>
-              <code>原載於Medium。原文鏈接： https://link.medium.com/AwUPNZdb8Y</code>
+              <code>原載於Medium。原文鏈接：https://link.medium.com/AwUPNZdb8Y</code>
             </Segment>
             <Button 
               color='blue'
@@ -78,32 +78,59 @@ export default ({ data }) => {
               读
             </Button>{" "}
             <Divider />
-            <Header as="h3" size="medium">
-              Sub-heading
-            </Header>
-            <p>
-              Cum sociis natoque penatibus et magnis dis parturient montes,
-              nascetur ridiculus mus.
-            </p>
 
-            <p>
-              Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem
-              malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus
-              commodo, tortor mauris condimentum nibh, ut fermentum massa.
-            </p>
-            <Header size="medium">Sub-heading</Header>
-            <p>
-              Cum sociis natoque penatibus et magnis dis parturient montes,
-              nascetur ridiculus mus. Aenean lacinia bibendum nulla sed
-              consectetur. Etiam porta sem malesuada magna mollis euismod.
-              Fusce dapibus, tellus ac cursus commodo, tortor mauris
-              condimentum nibh, ut fermentum massa justo sit amet risus.
-            </p>
-            <Button basic circular size="huge">
-              <a href="#root">Next</a>
-            </Button>
+            <Header size="large" as="h2">
+              <Header.Content>最大條道理可以寫廣東話嘅文體</Header.Content>
+              <Label as='a' color='teal'>
+                粵語
+              </Label>
+              <Label as='a' color='white'>
+                正體字
+              </Label>
+            </Header>
+            <Header.Subheader>阿擇 （Chaaak） 2019.2.24</Header.Subheader>
             <Divider hidden />
+            <blockquote>
+              兒童書你都唔用全粵語，唔通連聽個故仔都要大人逐字翻譯？
+            </blockquote>
+            <Segment secondary>
+              <code>原載於Medium。原文鏈接：https://link.medium.com/AwUPNZdb8Y</code>
+            </Segment>
+            <Button 
+              color='blue'
+              as={Link}
+              to='/blog/write_cantonese'>
+              读
+            </Button>{" "}
+            <Divider />
+            
+            <Header size="large" as="h2">
+              <Header.Content>阿擇粵語文章合集</Header.Content>
+              <Label as='a' color='teal'>
+                粵語
+              </Label>
+              <Label as='a' color='white'>
+                正體字
+              </Label>
+            </Header>
+            <Header.Subheader>阿擇 （Chaaak） 2019.2.24</Header.Subheader>
+            <Divider hidden />
+            <blockquote>
+              不斷更新
+            </blockquote>
+            <Segment secondary>
+              <code>原載於Medium。原文鏈接：https://link.medium.com/TMFfMh1I8Y</code>
+            </Segment>
+            <Button 
+              color='blue'
+              as={Link}
+              to='/blog/write_cantonese'>
+              读
+            </Button>{" "}
+            <Divider />
           </Grid.Column>
+
+
           <Grid.Column width={4} floated="right">
             <Segment secondary>
               <Header as="h4">杂谈</Header>
@@ -116,18 +143,6 @@ export default ({ data }) => {
               <List.Item as="a">Septempber 2017</List.Item>
               <List.Item as="a">August 2017</List.Item>
               <List.Item as="a">June 2017</List.Item>
-              <List.Item as="a">April 2017</List.Item>
-              <List.Item as="a">March 2017</List.Item>
-              <List.Item as="a">February 2017</List.Item>
-              <List.Item as="a">January 2017</List.Item>
-              <List.Item as="a">December 2016</List.Item>
-              <List.Item as="a">October 2016</List.Item>
-              <List.Item as="a">July 2016</List.Item>
-              <List.Item as="a">July 2016</List.Item>
-              <List.Item as="a">May 2016</List.Item>
-              <List.Item as="a">April 2016</List.Item>
-              <List.Item as="a">January 2016</List.Item>
-              <List.Item as="a">May 2015</List.Item>
             </List>
             <Header as="h4">Elsewhere</Header>
             <List>
@@ -141,24 +156,3 @@ export default ({ data }) => {
     </PageLayout>
   )
 }
-
-export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
